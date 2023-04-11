0170ff632d0f4a58c81047533515160310f6f9fc
Carlos Gaffo <carlos.gaffo@live.com>
2023-04-10 21:45:32 -0300

    add exercicios 8 ao 10

---
+++ exerc8.py
@@ -0,0 +1,10 @@
+def ler_temperatura():
+    temperatura = float(input("Digite a temperatura em graus Celsius: "))
+    return temperatura
+
+def converter(temperatura):
+    temperatura_fahrenheit = (9 * temperatura + 160) / 5
+    return temperaturaConvertida
+
+def mostrar(temperatura_fahrenheit):
+    print(f"A temperatura em graus Fahrenheit é: {temperatura_fahrenheit:.2f}")
